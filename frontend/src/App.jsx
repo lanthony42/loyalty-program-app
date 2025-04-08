646b8331d64c46b0afade2f15ce64fea9c6bc8be
lanthony42 <7217219+lanthony42@users.noreply.github.com>
2025-04-08 15:42:07 -0400

    Added transaction page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,32 +7,36 @@ import Login from "@/pages/Accounts/Login";
 import EditProfile from "@/pages/Accounts/EditProfile";
 import Register from "@/pages/Accounts/Register";
 import Transactions from "@/pages/Transactions";
+import Transaction from "@/pages/Transactions/Transaction";
 import NotFound from "@/pages/NotFound";
 
 const MyRoutes = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/edit-profile" element={<EditProfile />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/transactions" element={<Transactions />} />
-        <Route path="*" element={<NotFound />} />
-      </Route>
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path="login" element={<Login />} />
+                <Route path="dashboard" element={<Dashboard />} />
+                <Route path="edit-profile" element={<EditProfile />} />
+                <Route path="register" element={<Register />} />
+                <Route path="transactions">
+                    <Route index element={<Transactions />} />
+                    <Route path=":transactionId" element={<Transaction />} />
+                </Route>
+                <Route path="*" element={<NotFound />} />
+            </Route>
+        </Routes>
+    );
 };
 
 const App = () => {
-  return (
-      <BrowserRouter>
-          <AuthProvider>
-              <MyRoutes />
-          </AuthProvider>
-      </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <AuthProvider>
+                <MyRoutes />
+            </AuthProvider>
+        </BrowserRouter>
+    );
 };
 
 export default App;
